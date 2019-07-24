074a504a63b894c2fa64c5c72043a0cd24bd47ea
cartacode <cartacode@outlook.com>
2019-07-24 09:14:28 -0400

    create a api wrapper class for float.com

--- slackbot.py
+++ slackbot.py
@@ -1,4 +1,5 @@
 import os
+import requests
 import pytz
 from simple_salesforce import Salesforce
 import datetime
@@ -8,12 +9,47 @@ load_dotenv()
 
 if os.environ.get('SALESFORCE_URL') is not None:
     SALESFORCE_URL = os.environ.get('SALESFORCE_URL')
+    FLOAT_API_KEY = os.environ.get('FLOAT_API_KEY')
+    FLOAT_API_URL = os.environ.get('FLOAT_API_URL')
 else:
     SALESFORCE_URL = 'SALESFORCE DOMAIN'
 
-if __name__ == "__main__":
-    sf = Salesforce(instance='SALESFORCE_URL', session_id='00D30000001ICYF!AQ4AQAR_2a_pRTrbUPlMdrvogVcxK7_Tc0gMu3uz8Sz0T5wB_OCRpvpgunOa7OAt2oh0wVT2FbEvw9EJG7OGto8V5ETGcqQ2')
 
-    end = datetime.datetime.now(pytz.UTC)
-    records = sf.Contact.updated(end - datetime.timedelta(days=10), end)
-    print records
+class FloatAPI:
+    """
+    api wraper for FLOAT.COM
+    """
+    def __init__(self):
+        self.url = FLOAT_API_URL
+        self.access_key = FLOAT_API_KEY
+
+    def get_people(self):
+        headers={
+            'Authorization':'Bearer {}'.format(self.FLOAT_API_KEY),
+            'User-Agent':'MedmetryPyFloat',
+            'Content-Type':'application/json',
+            'Accept':'application/json'
+        }
+
+        pplresp = requests.get(self.url+'/people',headers=headers)
+        print(pplresp.body)
+
+
+
+class ScheduleBot:
+    """
+    slackbot class
+    """
+    def __init__(self, session_id):
+        self.sf = Salesforce(instance=SALESFORCE_URL, session_id=session_id)
+
+    def test_salesforce(self):
+        end = datetime.datetime.now(pytz.UTC)
+        records = self.sf.Contact.updated(end - datetime.timedelta(days=10), end)
+        print(records)
+
+
+if __name__ == "__main__":
+    bot = ScheduleBot('session_id')
+    float_api = FloatAPI()
+    .get_people()
